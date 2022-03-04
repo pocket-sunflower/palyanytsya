9d0ea896d558509a72022eb3fc5415a2d65793ea
Pocket Sunflower <russian.warship@fuck.yourself>
2022-03-04 15:04:43 +0200

    Add per-architecture build scripts for Mac

--- build_scripts_PyInstaller/build_for_mac_m1.py
+++ build_scripts_PyInstaller/build_for_mac_m1.py
@@ -1,7 +1,7 @@
 from build_base import build
 
 if __name__ == '__main__':
-    build(output_folder_name="Mac",
+    build(output_folder_name="Mac (M1)",
           target_platform_name="macOS",
           python_platform_check_string="darwin",
           spec_files=[
