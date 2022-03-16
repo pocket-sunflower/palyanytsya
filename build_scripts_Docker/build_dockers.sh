04c1f320598a1ce115d5a7dc0785cd299b2596c9
Pocket Sunflower <russian.warship@fuck.yourself>
2022-03-17 00:28:01 +0200

    Update Docker build files
    Switched to GitHub Container Registry.

--- build_scripts_Docker/build_dockers.sh
+++ build_scripts_Docker/build_dockers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ..
-docker build --rm -f build_scripts_Docker/Dockerfile_palyanytsya -t pocketsunflower/palyanytsya:latest .
-docker build --rm -f build_scripts_Docker/Dockerfile_pyrizhok -t pocketsunflower/pyrizhok:latest .
+docker build --rm -f build_scripts_Docker/Dockerfile_palyanytsya -t ghcr.io/pocket-sunflower/palyanytsya:latest .
+docker build --rm -f build_scripts_Docker/Dockerfile_pyrizhok -t ghcr.io/pocket-sunflower/pyrizhok:latest .
