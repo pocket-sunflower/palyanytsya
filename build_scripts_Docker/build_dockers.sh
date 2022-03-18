2cca74ebc863f44d7bebcce12f6e22d91d950a62
Pocket Sunflower <russian.warship@fuck.yourself>
2022-03-18 02:25:36 +0200

    WIP on CI

--- build_scripts_Docker/build_dockers.sh
+++ build_scripts_Docker/build_dockers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ..
-docker build --rm -f build_scripts_Docker/Dockerfile_palyanytsya -t ghcr.io/pocket-sunflower/palyanytsya:latest .
-docker build --rm -f build_scripts_Docker/Dockerfile_pyrizhok -t ghcr.io/pocket-sunflower/pyrizhok:latest .
+pwd
+docker build -f build_scripts_Docker/Dockerfile_palyanytsya -t ghcr.io/pocket-sunflower/palyanytsya:latest .
+docker build -f build_scripts_Docker/Dockerfile_pyrizhok -t ghcr.io/pocket-sunflower/pyrizhok:latest .
