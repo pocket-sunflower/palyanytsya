633f4594f34779675f10b65207df862029dcb770
Pocket Sunflower <russian.warship@fuck.yourself>
2022-03-18 03:33:59 +0200

    WIP on CI

--- build_scripts_Docker/build_dockers.sh
+++ build_scripts_Docker/build_dockers.sh
@@ -5,5 +5,5 @@ SCRIPT_PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$SCRIPT_PARENT_PATH/.."
 
 # build the containers
-docker build -f build_scripts_Docker/Dockerfile_palyanytsya -t ghcr.io/pocket-sunflower/palyanytsya:latest .
+#docker build -f build_scripts_Docker/Dockerfile_palyanytsya -t ghcr.io/pocket-sunflower/palyanytsya:latest .
 docker build -f build_scripts_Docker/Dockerfile_pyrizhok -t ghcr.io/pocket-sunflower/pyrizhok:latest .
