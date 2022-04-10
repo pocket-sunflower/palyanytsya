a59757786a1edcf957d1f19e54f3f059cd65beb3
Pocket Sunflower <russian.warship@fuck.yourself>
2022-04-10 17:58:23 +0300

    Add coloredlogs

--- utils/logs.py
+++ utils/logs.py
@@ -1,6 +1,15 @@
 import logging
 
+import coloredlogs
+
 logging.basicConfig(format='[%(asctime)s - %(levelname)s] %(message)s',
                     datefmt="%H:%M:%S")
 logger = logging.getLogger("PALYANYTSYA")
+coloredlogs.install(
+    logger=logger,
+    level=logging.INFO,
+    fmt="{asctime} {name} {process} [{levelname}] {message}",
+    style="{",
+    datefmt="%H:%M:%S"
+)
 logger.setLevel("INFO")
